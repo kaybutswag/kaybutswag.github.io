cfe29e0d5a7489918aa871d1041e19ecfd6bd42b
kaybutswag <kbwagner426@gmail.com>
2018-04-06 17:31:21 -0500

    project updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,34 @@
 var modalContent=
-  [{"h2":"PolitiPurple",
+  [{"h2":"Kinnections",
+    "h3":"Back-End Developer on Four Member Team",
+    "img":"assets/images/kinections.png",
+    "p":"User Friendly Demo in Progress: We have created an app that matches individuals looking for a work out buddy. Users can fill out their information, review potential matches, chat, and make plans!",
+    "a":"https://kinections-demo.herokuapp.com/"
+  },{"h2":"Mentor Match",
+    "h3":"React Developer on Three Member Team",
+    "img":"assets/images/guitar.png",
+    "p":"Coming Soon: This was made with guitar beginners in mind, to learn guitar chords, sample them, and see how tabs are written. User can also create own projects by writing tabs.",
+    "a":"#"
+  },{"h2":"My Book Club",
+    "h3":"Personal Project: Full Stack",
+    "img":"assets/images/book.png",
+    "p":"This React App allows users to create a reading list. User can get suggestions from this week's best seller list or search for any book in the NYT best seller history. User can save, unsave, and remove books.",
+    "a":"https://murmuring-reef-18520.herokuapp.com/"
+  },{"h2":"PolitiPurple",
     "h3":"Front End Developer on Four Member Team.",
     "img":"assets/images/news.png",
     "p":"Our team wanted to build an app to make readers think about their news sources. A user can search a term and select one of 12 sources. The app will search news from the last 30 days and return most relevant story from user's source and a source with an opposite lean. The sources have been paired based on <a target='_blank' href='http://www.allgeneralizationsarefalse.com/the-chart-version-3-0-what-exactly-are-we-reading/'>Media Bias Chart.</a> The app also shows readers how many likes the article received on Facebook (Note: Webhose API was used for likes and has a limited amount of hits).",
     "a":"https://kaybutswag.github.io/PolitiPurple/"
-  },{"h2":"Mentor Match",
-    "h3":"Personal Project: Full Stack",
-    "img":"assets/images/mentor.png",
-    "p":"The original objective was to match a user with friends. My mentor app stores user information and answers with local API. The user is then matched with a cast member of the West Wing. This app was deployed on Heroku and built with NPM Express Server.",
-    "a":"https://enigmatic-lowlands-63180.herokuapp.com/"
-  },{"h2":"Order Up!",
-    "h3":"Personal Project: Full Stack",
-    "img":"assets/images/burger.png",
-    "p":"This app uses NPM mysql to keep track of orders and their status. User can also dynamically add orders, which will be stored server-side.",
-    "a":"https://immense-mesa-59802.herokuapp.com/"
   },{"h2":"All Aboard!",
     "h3":"Personal Project: Full Stack",
     "img":"assets/images/train.png",
     "p":"This app uses firebase for server-side storage to keep track of trains as populated by user. It can calculate the time for the next train and how many minutes until that train.",
     "a":"https://kaybutswag.github.io/TrainScheduler/"
-  },{"h2":"Kinnections",
-    "h3":"Back-End Developer on Four Member Team",
-    "img":"assets/images/LandingPageWhite_loginmoved copy.png",
-    "p":"Our goal is to create an app that matches individuals looking for a work out buddy. Users can fill out their information, review potential matches, chat, and make plans!",
-    "a":"#"
-  },{"h2":"Snark-Off",
-    "h3":"Personal Project",
-    "img":"assets/images/snark.png",
-    "p":"A pop culture take on a fighter game. Each character has individual health points that decrease as your fighter and adversary insult each other.",
-    "a":"https://kaybutswag.github.io/week4Game/"
+  },{"h2":"Mentor Match",
+    "h3":"Personal Project: Full Stack",
+    "img":"assets/images/mentor.png",
+    "p":"The original objective was to match a user with friends. My mentor app stores user information and answers with local API. The user is then matched with a cast member of the West Wing. This app was deployed on Heroku and built with NPM Express Server.",
+    "a":"https://enigmatic-lowlands-63180.herokuapp.com/"
   },{"h2":"Citizenship Test",
     "h3":"Personal Project",
     "img":"assets/images/test.png",
@@ -144,7 +144,7 @@ function fillModal(number){
   myImg.attr({"src": modalContent[number].img,"alt":modalContent[number].h2});
 
 
-  if(number===4){
+  if(number===1){
     var myBlock=$("<div>")
   myBlock.append("<h2>"+modalContent[number].h2+"</h2><h3>"+modalContent[number].h3+"</h3><p>"+modalContent[number].p+"</p>"+
     "<a class='button' id='launchButton' type='submit' href="+modalContent[number].a+" target='_blank'>Coming Soon!</a>");
@@ -162,7 +162,16 @@ function fillModal(number){
   
 }
 
-
-
 });
+var mq1 = window.matchMedia( "(max-width: 550px)" );
+  if (mq1.matches){  
+    $(".abme").empty();
+    $(".abme").append("<div class='profbox2'<p id='abme1'> Before diving into the depths of web development, I worked as an economic litigation consultant ready to make a career change. Translation: I transformed messy data into an argument through analyses and regressions. Along the way, I had glimpses of powerful tools just out of my reach: data scraping, API's, and machine learning. I envied the ability to create. After three and a half years, I decided I was ready to make a change.</p></div><div class='profbox1 funbox2'><img id='profpic' src='assets/images/2512CLARO-wagner_275x250.png'><img id='funpic' src='assets/images/KatAndCam.png'></div><div class='funbox1'><p id='abme2'> Beyond an enhancement of my data experience, I wanted to understand the tools I took for granted, from <span style= 'font-weight: 700;'>Data to Design</span>. My projects are the products of that desire. I primarily code in Javascript, recently focusing on React. Also in my toolbox, JQuery, HTML, CSS, Mongo, Python, SAS and Handlebars. As of now, I have more Frontend experience, but adding to my Backend resume regularly. Look around and let me know what you think! </p></div>");
+  } 
+
+var mq2 = window.matchMedia( "(min-width: 551px)" );
+  if (mq2.matches){  
+    $(".abme").empty();
+    $(".abme").append("<div class='three columns profbox1'><img id='profpic' src='assets/images/2512CLARO-wagner_275x250.png'></div><div class='three columns profbox2'><p id='abme1'> Before diving into the depths of web development, I worked as an economic litigation consultant ready to make a career change. Translation: I transformed messy data into an argument through analyses and regressions. Along the way, I had glimpses of powerful tools just out of my reach: data scraping, API's, and machine learning. I envied the ability to create. After three and a half years, I decided I was ready to make a change.</p></div><div class='three columns funbox1'><p id='abme2'> Beyond an enhancement of my data experience, I wanted to understand the tools I took for granted, from <span style= 'font-weight: 700;'>Data to Design</span>. My projects are the products of that desire. I primarily code in Javascript, recently focusing on React. Also in my toolbox, JQuery, HTML, CSS, Mongo, Python, SAS and Handlebars. As of now, I have more Frontend experience, but adding to my Backend resume regularly. Look around and let me know what you think! </p></div><div class='three columns funbox2'><img id='funpic' src='assets/images/KatAndCam.png'></div>");
+  } 
 
